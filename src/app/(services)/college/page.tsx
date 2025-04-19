83be86fc704e4a377414da14a2264d9b4f5f56a7
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-04-19 09:02:58 +0530

    init college, corporate

--- src/app/(services)/college/page.tsx
+++ src/app/(services)/college/page.tsx
@@ -1,9 +1,28 @@
+import Marquee from "@/app/(components)/marqueee"
+import { Button } from "@/components/ui/button"
+
 const College = () => {
     return (
         <div>
-            College
+            <div className="h-[84vh] lg:h-[80vh]">
+                <div className="lg:w-[50vw] h-full flex flex-col items-center justify-center">
+                    <h1 className="text-[5rem] lg:text-[8rem] font-bold">COLLEGE</h1>
+                    <p className="text-center font-light w-[80%] lg:w-[90%]">Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis.</p>
+                    <br />
+                    {/* <br /> */}
+                    <Button className="border">
+                        Learn More
+                    </Button>
+                </div>
+                <div>
+                    {/* image */}
+                </div>
+            </div>
+            <Marquee items={['XCELL', 'MPOWER', 'U-TURN', 'SPARKLE', 'CHECKMATE', 'CORPORATE READINESS PROGRAM']} />
+
         </div>
     )
+
 }
 
 export default College
--- src/app/(services)/corporate/page.tsx
+++ src/app/(services)/corporate/page.tsx
@@ -1,7 +1,25 @@
+import Marquee from "@/app/(components)/marqueee"
+import { Button } from "@/components/ui/button"
+
 const Corporate = () => {
     return (
         <div>
-            Corporate
+            <div className="h-[84vh] lg:h-[80vh]">
+                <div className="lg:w-[50vw] h-full flex flex-col items-center justify-center">
+                    <h1 className="text-[5rem] lg:text-[8rem] font-bold">CORPORATE</h1>
+                    <p className="text-center font-light w-[80%] lg:w-[90%]">Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis.</p>
+                    <br />
+                    {/* <br /> */}
+                    <Button className="border">
+                        Learn More
+                    </Button>
+                </div>
+                <div>
+                    {/* image */}
+                </div>
+            </div>
+            <Marquee items={['SYNERGY', 'EMOTIONAL INTELLIGENCE', 'EFFECTIVE COMMUNICATION', 'FINE TUNE YOURSELF', 'PROFESSIONALISM']} />
+
         </div>
     )
 }
