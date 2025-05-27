1ab5881413f18f79b7104ab058f4193d4bd9092a
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-05-28 00:26:26 +0530

    services under school

--- src/app/(services)/school/page.tsx
+++ src/app/(services)/school/page.tsx
@@ -7,7 +7,7 @@ import PI from "../../../Assets/Placeholder Image.webp"
 const School = () => {
     return (
         <div>
-            <section className="h-[84vh] lg:h-[80vh]">
+            <section className="flex h-[84vh] lg:h-[80vh]">
                 <div className="lg:w-[50vw] h-full flex flex-col items-center justify-center">
                     <h1 className="text-[5rem] lg:text-[8rem] font-bold">SCHOOL</h1>
                     <p className="text-center font-light w-[80%] lg:w-[90%]">Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis.</p>
@@ -17,8 +17,12 @@ const School = () => {
                         Learn More
                     </Button>
                 </div>
-                <div>
-                    {/* image */}
+                <div className="flex items-center justify-center h-full w-[50vw]">
+                    <img
+                        src={PI.src}
+                        alt="school"
+                        className="object-cover rounded-2xl opacity-[0.4] h-[90%] w-[90%]"
+                    />
                 </div>
             </section>
             <Marquee items={['WINNING EDGE', 'COUNSELING', 'SPELL-BINDERS', 'CONSULTANCY']} />
@@ -46,20 +50,22 @@ const School = () => {
                 </div>
             </div> */}
 
-            <section className="mt-[5vh] h-fit w-full border text-center">
-                <h1>SERVICES</h1>
+            <section className="mt-[5vh] h-fit w-full text-center">
+                <h1 className="text-[3rem] font-bold mb-[2rem]">SERVICES</h1>
                 <div className="flex flex-col items-center justify-between w-full">
                     <img
                         src={PI.src}
                         alt="school"
-                        className="object-cover rounded-2xl opacity-[0.4] h-[500px]"
+                        className="object-cover rounded-2xl opacity-[0.4] h-[500px] w-[90%]"
                     />
-                    <div className="relative z-10 mt-[2rem]">
-                        <h1>WINNING EDGE</h1>
-                        <p>Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis.</p>
+                    <div className="relative z-10 mt-[2rem] flex flex-col items-center">
+                        <h1 className="text-[2rem] font-bold">WINNING EDGE</h1>
+                        <p className="text-[1.2rem] w-[90%] font-light">Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis. Lorem ipsum dolor sit amet. Qui architecto maiores ea eligendi galisum sed natus aliquam est consequatur dolor ut internos Quis.</p>
                     </div>
                 </div>
-                <div>
+                <div className="mt-[5vh] flex justify-around">
+                    <ServiceCard />
+                    <ServiceCard />
                     <ServiceCard />
                 </div>
             </section>
